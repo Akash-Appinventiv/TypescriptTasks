493ec40dc1955f6d72783370c7056cbd04aec3a9
admin <admin@Akash-MacBook-Pro.local>
2022-07-07 22:17:30 +0530

    first commit

---
+++ src/utils/strings.ts
@@ -0,0 +1,29 @@
+import { images } from "."
+
+export const strings = {
+    postHeader: "Weekly Summit and Audit by Our Guest Swami Shankar",
+    live: "Live",
+    time: "On 23 Oct, 5:30 PM",
+    profileName: "Dipankar Mukherjee Biswas",
+    profileDesignation: "General Secretary - Noida, Uttar Pradesh",
+    host: "EVENT HOST",
+    speaker: "OUR SPEAKERS",
+}
+
+export const speakerDetails=[
+    {
+        image: images.profile,
+        name: "Anchal Srivastava",
+        designation: "State President | Noida, Uttar Prad…",
+    },
+    {
+        image: images.profile,
+        name: "Akansha",
+        designation: "National President",
+    },
+    {
+        image: images.profile,
+        name: "Krishi Mahajan",
+        designation: "GUEST",
+    }
+]
